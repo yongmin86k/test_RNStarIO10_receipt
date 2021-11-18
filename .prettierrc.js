ab0bf586fe30378c44bc5a8da30da8657bb33fab
Yongmin Kim <yongmin86k@gmail.com>
2021-11-17 21:24:35 -0800

    init

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  semi: false,
+  singleQuote: true,
+  bracketSpacing: true,
+  bracketSameLine: false,
+};
